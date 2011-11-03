b0cefe883aecc2f518c98bac0e7e54df9041bf34
rails-learner <ashokjaince@gmail.com>
2011-11-04 00:28:15 +0530

    finshed with layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+
+ def title
+base_title = "Clone of Twitter" 
+
+ if @title.nil?
+base_title
+else
+"#{base_title} | #{@title}"
+ end
+
+ end
+
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Twit::Application.routes.draw do
+  get "users/new"
+
+match '/contact', :to => 'pages#contact'
+match '/about', :to => 'pages#about'
+match '/help', :to => 'pages#help'
+match '/signup', :to =>"users#new"
+match '/', :to =>'pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +55,7 @@ Twit::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
